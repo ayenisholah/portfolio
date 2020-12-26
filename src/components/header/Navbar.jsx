c20523c9e870bbd71483cb205a720064227669c0
Shola Ayeni <ayenisholah@yahoo.com>
2020-12-26 11:36:20 +0100

    removed scroll

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,6 @@
 import Burger from "./Burger";
 import logo from "../../assets/logo.jpg"
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 
 export default function Navbar() {
     return (
--- src/components/header/RightNav.jsx
+++ src/components/header/RightNav.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 import about from "../../assets/about.svg"
 import experience from "../../assets/experience.svg"
 import contact from "../../assets/contact.svg"
